99f4adda5a8fb46a813e996a657df7f8ec0a8302
Aurélien Pupier <apupier@redhat.com>
2021-07-08 11:36:03 +0200

    WIP FUSETOOLS2-226 code coverage
    got coverage only for files in test folder and not real code...
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/index.ts
+++ src/test/index.ts
@@ -2,7 +2,11 @@ import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
+const NYC = require('nyc');
+
 export function run(): Promise<void> {
+	const nyc = setupCoverage();
+
 	// Create the mocha test
 	const mocha = new Mocha({
 		ui: 'bdd',
@@ -33,7 +37,42 @@ export function run(): Promise<void> {
 				});
 			} catch (err) {
 				e(err);
+			} finally {
+				if (nyc) {
+					nyc.writeCoverageFile();
+					nyc.report();
+				}
 			}
 		});
 	});
 }
+function setupCoverage() {
+	console.log('cwd used: ' + path.join(__dirname, '..', '..', '..'));
+	const nyc = new NYC({
+		extends: "@istanbuljs/nyc-config-typescript",
+		cache: false,
+		cwd: path.join(__dirname, '..', '..', '..'),
+		exclude: [
+			"**/**.test.js"
+		],
+		extension: [
+			".ts"
+		],
+		reporter: ['text', 'html'],
+		instrument: true,
+		hookRequire: true,
+		hookRunInContext: true,
+		hookRunInThisContext: true,
+		require: [
+			"ts-node/register",
+		],
+		sourceMap: true,
+		all: true,
+		excludeAfterRemap: false
+	});
+
+	nyc.reset();
+	nyc.wrap();
+	return nyc;
+}
+
